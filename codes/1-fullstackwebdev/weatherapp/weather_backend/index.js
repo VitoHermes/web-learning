38ed04d139b2de2ebd57c1b701dfe8f5b4b27b95
ChrisDavis <vito186cm@gmail.com>
2024-12-23 21:31:54 +1100

    add backend

---
+++ codes/1-fullstackwebdev/weatherapp/weather_backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const weatherRouter = require("./weatherRouter");
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+app.use('/api', weatherRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
